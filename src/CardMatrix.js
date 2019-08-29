4dd948a32e196cda9bcd571d3758d01bc67b8560
Viktor <viktor@visualcohol.com>
2019-08-29 18:49:46 +0200

    Card matrix refactored.

--- src/CardMatrix.js
+++ src/CardMatrix.js
@@ -3,47 +3,77 @@ import React, { useCallback, useState } from 'react';
 import './CardMatrix.scss';
 
 const CardMatrix = props => {
-  const [cardMatrixHeight, setCardMatrixHeight] = useState(null);
+  // props.count : number of cards
 
+  const [cardMatrix, setCardMatrix] = useState(null);
+
+  // Referencing the matrix
   const cardMatrixEl = useCallback(node => {
     if (node !== null) {
-      setCardMatrixHeight(node.getBoundingClientRect().height);
+      setCardMatrix({ height: node.getBoundingClientRect().height });
     }
   }, []);
 
-  const numberPairs = [];
+  /**
+   * Finding pairs of numbers with which the input count is dividalbe as a whole number
+   */
+  function getDivisions(count) {
+    const divisionPairs = [];
 
-  for (let i = 1; i <= props.count; i++) {
-    if (props.count % i === 0) {
-      numberPairs.push([i, props.count / i]);
+    for (let i = 1; i <= props.count; i++) {
+      if (props.count % i === 0) {
+        divisionPairs.push([i, props.count / i]);
+      }
     }
+
+    return divisionPairs;
   }
 
-  const theNumberPair = numberPairs.find(numberPair => {
-    return numberPair[0] - numberPair[1] >= 0;
-  });
+  /**
+   * Finding the pair of numbers which are closest to each other
+   */
+  function getClosestPair(numberPairs) {
+    const closestPair = numberPairs.find(numberPair => {
+      return numberPair[0] - numberPair[1] >= 0;
+    });
 
-  const tr = [];
-  const td = [];
-
-  for (let i = 0; i < theNumberPair[0]; i++) {
-    td.push(<div className='td' key={i} />);
+    return closestPair;
   }
 
-  for (let i = 0; i < theNumberPair[1]; i++) {
-    tr.push(
-      <div className='tr' key={i}>
-        {td}
-      </div>
-    );
+  /**
+   * Generating matrix table
+   */
+  function generateMatrix(numberPair) {
+    const tr = [];
+    const td = [];
+
+    for (let i = 0; i < numberPair[0]; i++) {
+      td.push(<div className='td' key={i} />);
+    }
+
+    for (let i = 0; i < numberPair[1]; i++) {
+      tr.push(
+        <div className='tr' key={i}>
+          {td}
+        </div>
+      );
+    }
+
+    return tr;
   }
+
+  const divisions = getDivisions(props.count);
+  const closestPair = getClosestPair(divisions);
 
   return (
     <div className='card-matrix' ref={cardMatrixEl}>
       <div
         className='table'
-        style={{ height: cardMatrixHeight - 2, width: cardMatrixHeight - 2 }}>
-        {tr}
+        style={{
+          height: cardMatrix ? cardMatrix.height - 2 : null,
+          width: cardMatrix ? cardMatrix.height - 2 : null
+        }}>
+        {generateMatrix(closestPair)}
       </div>
     </div>
   );
