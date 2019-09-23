a88fe143e51ed6416caf63750613350039c865fd
Viktor <viktor@visualcohol.com>
2019-09-23 17:04:41 +0200

    Docs updated.

---
+++ docs/precache-manifest.7c167a7c49c415a2bd4bd6cf8b2b459c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4de2a4b20bac5296e42a8512871e19f1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "cc1f42b78c0b727f5f9a",
+    "url": "./static/css/main.e32ace26.chunk.css"
+  },
+  {
+    "revision": "4d72c5d839f000b2c83b",
+    "url": "./static/js/2.b690a490.chunk.js"
+  },
+  {
+    "revision": "cc1f42b78c0b727f5f9a",
+    "url": "./static/js/main.1549ede7.chunk.js"
+  },
+  {
+    "revision": "c33a6ebe08797e9f0ee9",
+    "url": "./static/js/runtime-main.7b6e8a7f.js"
+  },
+  {
+    "revision": "cad0624f367302d7a16c193816f6a71a",
+    "url": "./static/media/icon_cards.cad0624f.svg"
+  }
+]);
